c3a3e9333c1e4fccb2c50eb35f34534e51a04ca4
Nshstephan <nshstephan@gmail.com>
2021-03-21 17:23:49 +0400

    bug fixed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else {
             callback(undefined,
                 body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " +
-                body.current.feelslike + "degrees out. The humidity is " + body.current.humidity + "%.");
+                body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + "%.");
         }
     });
 }
